56b111b65363266e023f75f8341c78629ca1755c
Kalinin <9776020@mail.ru>
2023-02-27 00:39:36 +0300

    Lection_Yask_3

---
+++ 1_3/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {5, 2, 3, 4, 5, 6, 7, 8, 9};
+int n = array.Length;
+int find = 5;
+int index = 0;
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  //index = index + 1
+  index++;
+};
